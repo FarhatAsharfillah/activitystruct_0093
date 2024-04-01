e03de42652c4b2b87b1b3b85375e6f2d844601c8
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-04-01 16:30:30 +0700

    commit 30 perbaikan input getline menjad cin

--- activitystruct_0093/activitystruct_0093.cpp
+++ activitystruct_0093/activitystruct_0093.cpp
@@ -18,9 +18,10 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cout << "Data ke-" << (i + 1) << ":" << endl;
 		cout << "Nomor Mahasiswa :";
-		getline(cin, mhs[i].nim);
+		cin >> mhs[i].nim;
 		cout << "Nama Mahasiswa : ";
-		getline(cin, mhs[i].nama);
+		cin >> mhs[i].nama;
+
 		cout << "Alamat Mahasiswa : " << endl;
 		cout << "\t Nama Desa : ";
 		cin >> mhs[i].alamat.desa;
@@ -28,18 +29,17 @@ int main() {
 		cin >> mhs[i].alamat.kota;
 		cout << "Umur Mahasiswa : ";
 		cin >> mhs[i].umur;
-		cin.ignore(1,'\n');
-		cout<<endl
+		cin.ignore(1, '\n');
+		cout << endl;
+}
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data Mahasiswa Ke-" << (i + 1) << ":" << endl;
+		cout << "\n NIM : " << mhs[i].nim;
+		cout << "\n Nama :" << mhs[i].nama;
+		cout << "\n Alamat : ";
+		cout << "\n \t Desa : " << mhs[i].alamat.desa;
+		cout << "\n \t Kota : " << mhs[i].alamat.kota;
+		cout << "\n Umur : " << mhs[i].umur;
+		cout << endl;
 	}
-}
-for (int i = 0; i < 3; i++) {
-	cout << endl;
-	cout << "Data Mahasiswa Ke-" << (i + 1) << ":" << endl;
-	cout << "\n NIM : " << mhs[i].nim;
-	cout << "\n Nama :" << mhs[i].nama;
-	cout << "\n Alamat : ";
-	cout << "\n \t Desa : " << mhs[i].alamat.desa;
-	cout << "\n \t Kota : " << mhs[i].alamat.kota;
-	cout << "\n Umur : " << mhs[i].umur;
-	cout << endl;
-}
